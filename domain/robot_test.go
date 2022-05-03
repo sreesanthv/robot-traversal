474518b8f60f66ec959b002b850f945d5ef97cd9
Sreesanth Vazhayil <sreesanthv@gmail.com>
2022-05-03 06:29:36 +0530

    Update robot_test.go

--- domain/robot_test.go
+++ domain/robot_test.go
@@ -15,4 +15,17 @@ func TestProcessCommand(t *testing.T) {
 	if robot.Direction != "N" {
 		t.Errorf("Invalid direction. Expected %s but got %s", "N", robot.Direction)
 	}
+
+	robot = NewRobot(NewPostion(0, 0), NewPostion(4, 4), "E")
+	robot.ProcessCommand("MLMLMLMRRMRMRMRM")
+
+	if robot.Postion.X != 0 {
+		t.Errorf("Invalid X position. Expected %d but got %d", 0, robot.Postion.X)
+	}
+	if robot.Postion.Y != 0 {
+		t.Errorf("Invalid Y position. Expected %d but got %d", 0, robot.Postion.Y)
+	}
+	if robot.Direction != "W" {
+		t.Errorf("Invalid direction. Expected %s but got %s", "W", robot.Direction)
+	}
 }
